1a3f0727e61f4a3d878e013a32aed06160d1ae30
curiousignite <berkeerbagci@outlook.com>
2024-07-09 03:11:49 +0300

    Dark mode added, cookies added for dark mode.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+const darkMode = getCookie("darkMode");
+
+console.log(darkMode);
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
+if (darkMode == "true") {
+  document.querySelector(".switch__input").checked = true;
+  DarkReader.enable();
+} else {
+  document.querySelector(".switch__input").checked = false;
+  console.log("11");
+  DarkReader.disable();
+}
+const checkbox = document.querySelector(".switch__input");
+console.log(checkbox);
+checkbox.addEventListener("change", function () {
+  console.log(checkbox.checked);
+  if (this.checked) {
+    DarkReader.enable();
+    document.cookie = "darkMode=true; SameSite=Strict";
+  } else {
+    DarkReader.disable();
+    document.cookie = "darkMode=false SameSite=Strict";
+  }
+});
+
 var swiper = new Swiper(".slide-content", {
   slidesPerView: 3,
   spaceBetween: 50,
